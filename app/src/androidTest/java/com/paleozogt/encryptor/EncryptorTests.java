1baed6120fc193d26951f227e1996614176fb9c8
Aaron Simmons <asimmons@sensoryinc.com>
2016-08-02 17:20:45 -0600

    encryption instrumentation test

--- app/src/androidTest/java/com/paleozogt/encryptor/EncryptorTests.java
+++ app/src/androidTest/java/com/paleozogt/encryptor/EncryptorTests.java
@@ -1,16 +1,60 @@
 package com.paleozogt.encryptor;
 
-import android.support.test.runner.AndroidJUnit4;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 
 import junit.framework.Assert;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-@RunWith(AndroidJUnit4.class)
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+
+@RunWith(Parameterized.class)
 public class EncryptorTests {
+    @Parameterized.Parameters
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                { NoKeystoreEncryptor.class },
+                { KeystoreRsaEncryptor.class },
+                { KeystoreAesEncryptor.class }
+        });
+    }
+
+    Class<? extends Encryptor> clazz;
+    Encryptor encryptor;
+
+    public EncryptorTests(Class<? extends Encryptor> clazz) {
+        this.clazz= clazz;
+    }
+
+    @Before
+    public void setup() throws InstantiationException, IllegalAccessException, InvocationTargetException {
+        try {
+            encryptor= clazz.getDeclaredConstructor(Context.class).newInstance(InstrumentationRegistry.getTargetContext());
+        } catch (NoSuchMethodException e) {
+            encryptor= clazz.newInstance();
+        }
+    }
+
+    byte[] makePlainText(int numBytes) throws UnsupportedEncodingException {
+        return StringUtils.repeat('A', numBytes).getBytes("UTF-8");
+    }
+
     @Test
-    public void noop() {
-        Assert.assertTrue(true);
+    public void roundtrip() throws GeneralSecurityException, IOException {
+        encryptor.makeKey();
+        byte[] plaintext= makePlainText(1024);
+        byte[] ciphertext= encryptor.encrypt(plaintext);
+        byte[] roundtriptext= encryptor.decrypt(ciphertext);
+        Assert.assertTrue(ArrayUtils.isEquals(plaintext, roundtriptext));
     }
 }
