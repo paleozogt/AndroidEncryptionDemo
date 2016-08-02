38fd413c33ce6760c7aa2255c7b9fcebbd2adce2
Aaron Simmons <asimmons@sensoryinc.com>
2016-08-02 13:37:06 -0600

    adding launcher activity with fragment

---
+++ app/src/main/java/com/paleozogt/androidencryptiondemo/MainActivityFragment.java
@@ -0,0 +1,24 @@
+package com.paleozogt.androidencryptiondemo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainActivityFragment extends Fragment {
+    Logger logger= LoggerFactory.getLogger(getClass());
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
