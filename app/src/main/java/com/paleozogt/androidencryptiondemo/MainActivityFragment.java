cbb4626d09d7934847e5a04999f85478cf2a8539
Aaron Simmons <asimmons@sensoryinc.com>
2016-08-02 13:51:32 -0600

    stub buttons

--- app/src/main/java/com/paleozogt/androidencryptiondemo/MainActivityFragment.java
+++ app/src/main/java/com/paleozogt/androidencryptiondemo/MainActivityFragment.java
@@ -19,6 +19,51 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_main, container, false);
+
+        view.findViewById(R.id.gen_key).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                genKey();
+            }
+        });
+
+        view.findViewById(R.id.gen_plaintext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                genPlaintext();
+            }
+        });
+
+        view.findViewById(R.id.encrypt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                encrypt();
+            }
+        });
+
+        view.findViewById(R.id.decrypt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decrypt();
+            }
+        });
+
         return view;
+    }
+
+    protected void genKey() {
+        logger.debug("genKey");
+    }
+
+    protected void genPlaintext() {
+        logger.debug("genPlaintext");
+    }
+
+    protected void encrypt() {
+        logger.debug("encrypt");
+    }
+
+    protected void decrypt() {
+        logger.debug("decrypt");
     }
 }
