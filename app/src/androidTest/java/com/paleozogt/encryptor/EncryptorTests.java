b21cefeedfb6f58b422b0317905bb9805e5a45cd
Aaron Simmons <asimmons@sensoryinc.com>
2016-08-02 17:00:00 -0600

    wiring up instrumentation tests

---
+++ app/src/androidTest/java/com/paleozogt/encryptor/EncryptorTests.java
@@ -0,0 +1,16 @@
+package com.paleozogt.encryptor;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class EncryptorTests {
+    @Test
+    public void noop() {
+        Assert.assertTrue(true);
+    }
+}
